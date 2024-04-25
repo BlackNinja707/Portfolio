428ee675df067a194f91eb4b1cee98c550e552bb
AlterShadow <thomas0xken@gmail.com>
2024-04-25 17:37:54 -0400

    upload

---
+++ src/mock/profile.js
@@ -0,0 +1,23 @@
+export const SOCIAL_LINKS = [
+  {
+    link: 'https://twitter.com/ThomasKen111',
+    icon: 'akar-icons:twitter-fill',
+  },
+  {
+    link: 'https://github.com/altershadow',
+    icon: 'akar-icons:github-fill',
+  },
+  {
+    link: 'https://t.me/ThomasKen_111',
+    icon: 'akar-icons:telegram-fill',
+  },
+];
+
+export const HERO_TITLES = ['Thomas Ken', 'Full-stack & Blockchain Engineer', 'AI Enthusiast'];
+
+export const aboutParagraph =
+  'Highly motivated and enthusiastic Full Stack & Blockchain Engineer with experience in designing, developing and maintaining Web2 & Web3 platforms using technologies such as Solidity, Rust, React, Vue, Next.js';
+
+export const contactEmail = 'thomas0xken@gmail.com';
+
+export const documentTitle = 'Hello I am Thomas Ken | Full Stack & Blockchain Engineer | AI Enthusiast';
